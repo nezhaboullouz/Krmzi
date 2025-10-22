5213189bd0e678ffbc0c77071ae82d9da7f33c2d
nezhaboullouz <nezhaboulouz@icloud.com>
2025-10-22 11:20:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-    // script.js
-    
-    // Function to hide header and footer elements
-    function hideHeaderAndFooter() {
-        const style = document.createElement('style');
-        style.innerHTML = '#headerNav, footer { display: none !important; }';
-        document.head.appendChild(style);
-    }
-    
-   // Function to detect text copy and send a message to the native app
-   function setupTextCopyDetection() {
-       document.addEventListener('copy', function(e) {
-           // Ensure window.webkit.messageHandlers.textCopyHandler exists before posting
-           if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.textCopyHandler) {
-               window.webkit.messageHandlers.textCopyHandler.postMessage('textCopied');
-           }
-       });
-   }
-   
-   // Execute functions when the document is ready
-   document.addEventListener('DOMContentLoaded', function() {
-       hideHeaderAndFooter();
-       setupTextCopyDetection();
-   });
-   
-   // Also execute on document end in case DOMContentLoaded fires too early for some content
-   window.addEventListener('load', function() {
-   hideHeaderAndFooter();
+// script.js
+
+// Function to hide header and footer elements, and other unwanted elements
+function hideHeaderAndFooter() {
+    const style = document.createElement('style');
+    style.innerHTML = `
+        #headerNav,
+        footer,
+        header.header, /* Added to hide the header */
+        div.DownloadArea /* Added to hide the download area */
+        { display: none !important; }
+    `;
+    document.head.appendChild(style);
+}
+
+// Function to detect text copy and send a message to the native app
+function setupTextCopyDetection() {
+    document.addEventListener('copy', function(e) {
+        // Ensure window.webkit.messageHandlers.textCopyHandler exists before posting
+        if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.textCopyHandler) {
+            window.webkit.messageHandlers.textCopyHandler.postMessage('textCopied');
+        }
+    });
+}
+
+// Execute functions when the document is ready
+document.addEventListener('DOMContentLoaded', function() {
+    hideHeaderAndFooter(); // This function now includes the new hiding logic
+    setupTextCopyDetection();
+});
+
+// Also execute on document end in case DOMContentLoaded fires too early for some content
+window.addEventListener('load', function() {
+    hideHeaderAndFooter(); // This function now includes the new hiding logic
 });
